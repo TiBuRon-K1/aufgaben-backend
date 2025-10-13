3d1cc74d2b007db49af57ffc74e6d2c62afc6280
аше мя <you@example.com>
2025-10-13 13:53:31 +0200

    Add health endpoint directly in main class

--- src/main/java/com/example/aufgaben_backend/controller/HealthController.java
+++ src/main/java/com/example/aufgaben_backend/controller/HealthController.java
@@ -1,4 +1,13 @@
 package com.example.aufgaben_backend.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
+
+    @GetMapping("/health")
+    public String health() {
+        return "✅ Server is running!";
+    }
 }
