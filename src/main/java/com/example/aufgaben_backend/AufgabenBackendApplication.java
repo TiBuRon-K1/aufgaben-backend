e0c243eedba495506f775b9fb0d6a83f5fae965a
аше мя <you@example.com>
2025-10-13 14:31:33 +0200

    Fix duplicate /health mapping

--- src/main/java/com/example/aufgaben_backend/AufgabenBackendApplication.java
+++ src/main/java/com/example/aufgaben_backend/AufgabenBackendApplication.java
@@ -2,19 +2,11 @@ package com.example.aufgaben_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class AufgabenBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AufgabenBackendApplication.class, args);
-    }
-
-    @GetMapping("/health")
-    public String health() {
-        return "✅ Server is running!";
     }
 }
